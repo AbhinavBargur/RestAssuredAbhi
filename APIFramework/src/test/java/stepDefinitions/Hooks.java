3b1449cc23341aa832d0bdb3f7b18007ec51aa81
Abhinav Bargur <abhiviru93@gmail.com>
2020-07-21 18:33:17 +0100

    tagging features

---
+++ APIFramework/src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import java.io.IOException;
+
+import io.cucumber.java.Before;
+
+public class Hooks {
+	
+	@Before("@DeletePlace")
+	public void beforeScenario() throws IOException {
+		
+		StepDefinition sd = new StepDefinition();// Use hooks when you have to execute a method independently when it is dependent. And create object of step definition to access class.
+		sd.add_place_payload_with("bargur", "Spanish", "North");
+		sd.user_calls_with_http_request("AddPlaceAPI", "POST");
+		sd.verify_place_id_created_matches_with_using("bargur", "POST");
+	}
+
+}
