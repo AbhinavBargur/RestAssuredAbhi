451d905f092ce5505b31ac9ea4e91a6a87a864fb
Abhinav Bargur <abhiviru93@gmail.com>
2020-07-22 00:37:29 +0100

    small update

--- APIFramework/src/test/java/stepDefinitions/Hooks.java
+++ APIFramework/src/test/java/stepDefinitions/Hooks.java
@@ -12,7 +12,7 @@ public class Hooks {
 		StepDefinition sd = new StepDefinition();// Use hooks when you have to execute a method independently when it is dependent. And create object of step definition to access class.
 		sd.add_place_payload_with("bargur", "Spanish", "North");
 		sd.user_calls_with_http_request("AddPlaceAPI", "POST");
-		sd.verify_place_id_created_matches_with_using("bargur", "POST");
+		sd.verify_place_id_created_matches_with_using("bargur", "GET");
 	}
 
 }
